c0a40d30ef0698e21db5c6c5caf7ac47e6de32f4
sunilpandey6 <55409916+sunilpandey6@users.noreply.github.com>
2025-12-18 10:05:52 +0100

    update working with remote desktop

---
+++ Assets/Script/NewTest/TaskBar/MenuController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [Header("Task Bar")]
+    [SerializeField] private OVRCameraRig ovrRig;
+    [SerializeField] private GameObject panelPrefab;
+    [SerializeField] private float panelDistance = 1.5f;
+    [SerializeField] private float distanceY = -0.2f;
+    [SerializeField] private float ttl = 60f;
+
+    private float idleTimer = 0f;
+
+    private void Start() {
+        panelPrefab.SetActive(false);
+        StartCoroutine(ShowPanelWithDelay());
+    }
+
+    private IEnumerator ShowPanelWithDelay() {
+        yield return new WaitForSeconds(Random.Range(5f, 8f));
+        ShowPanel();
+    }
+
+    private void Update() {
+        if (OVRInput.GetDown(OVRInput.Button.Start)) {
+            Debug.Log("Start button pressed! Showing panel.");
+            ShowPanel();
+        } 
+        else {
+            idleTimer += Time.deltaTime;
+
+            if (idleTimer >= ttl) {
+                Debug.Log("TTL expired. Hiding panel.");
+                HidePanel();
+                idleTimer = 0f;
+            }
+        }
+    }
+
+    public void ShowPanel() {
+        Transform cam = ovrRig.centerEyeAnchor;
+
+        Vector3 spawnPos = cam.position + cam.forward * panelDistance;
+        spawnPos.y = cam.position.y + distanceY;
+
+        panelPrefab.transform.SetPositionAndRotation(
+            spawnPos,
+            Quaternion.LookRotation(cam.forward, cam.up)
+        );
+        panelPrefab.SetActive(true);
+        idleTimer = 0f;
+
+    }
+
+    private void HidePanel() {
+        panelPrefab.SetActive(false);
+    }
+}
